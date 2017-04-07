4005466e23e1d7b9c7c0dd7d6ef0d1b924d1aa20
pittcr3w <pittcrew@ksu.edu>
2017-04-07 17:14:32 -0500

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,13 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '/projects/');
+xhr.send(null);
+
+xhr.onreadystatechange = function (){
+  var DONE = 4;
+  var OK = 200;
+  if (xhr.readyState === DONE) {
+    if (xhr.status === OK) {
+      
+    }
+  }
+}
